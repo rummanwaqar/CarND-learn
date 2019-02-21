c3fd49454a4d9e9add56a65a04fbff268448666d
rummanwaqar <rumman.waqar05@gmail.com>
2019-02-21 00:06:28 -0700

    completed naive bayes

--- 36_prediction/src/classifier.cpp
+++ 36_prediction/src/classifier.cpp
@@ -1,35 +1,95 @@
-#include "classifier.h"
+  #include "classifier.h"
 
 GNB::GNB() {}
 
 void GNB::train(const std::vector<std::vector<double>> &data,
                 const std::vector<std::string> &labels) {
-  /**
-   * Trains the classifier with N data points and labels.
-   * @param data - array of N observations
-   *   - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
-   *   - Example : [[3.5, 0.1, 5.9, -0.02],
-   *                [8.0, -0.3, 3.0, 2.2],
-   *                 ...
-   *                ]
-   * @param labels - array of N labels
-   *   - Each label is one of "left", "keep", or "right".
-   *
-   * TODO: Implement the training function for your classifier.
-   */
+  // split data between features
+  std::vector<std::vector<std::vector<double>>> feature_data(possible_labels.size());
+  for(int i=0; i<labels.size(); i++) {
+    auto it = std::find(possible_labels.begin(), possible_labels.end(), labels[i]);
+    if(it != possible_labels.end()) {
+      int index = std::distance(possible_labels.begin(), it);
+      feature_data[index].push_back(data[i]);
+    } else {
+      std::cerr << "Unknown label" << std::endl;
+    }
+  }
+
+  // prior probability p(C_k) for each label
+  for(int i=0; i<possible_labels.size(); ++i) {
+    label_prob_.push_back(float(feature_data[i].size()) / labels.size());
+    std::cout << "p(" << possible_labels[i] << ") = " << label_prob_[i] << std::endl;
+  }
+
+  // calculate conditional probability for each label/feature p(feature | label)
+  // aka compute and store the mean and standard deviation of over all points of
+  // feature with label x
+
+  // feature: S
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::vector<double> v;
+    for(auto const& x : feature_data[i]) {
+      v.push_back(x[0]);
+    }
+    s_prob_.push_back(calc_gaussian(v));
+  }
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::cout << "p(s | " << possible_labels[i] << ") = N(" << s_prob_[i].mean << "," << s_prob_[i].stdev << ")" << std::endl;
+  }
+
+  // feature: D
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::vector<double> v;
+    for(auto const& x : feature_data[i]) {
+      v.push_back(x[1]);
+    }
+    d_prob_.push_back(calc_gaussian(v));
+  }
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::cout << "p(d | " << possible_labels[i] << ") = N(" << d_prob_[i].mean << "," << d_prob_[i].stdev << ")" << std::endl;
+  }
+
+  // features: D_dot
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::vector<double> v;
+    for(auto const& x : feature_data[i]) {
+      v.push_back(x[3]);
+    }
+    d_dot_prob_.push_back(calc_gaussian(v));
+  }
+  for(int i=0; i<possible_labels.size(); ++i) {
+    std::cout << "p(d_dot | " << possible_labels[i] << ") = N(" << d_dot_prob_[i].mean << "," << d_dot_prob_[i].stdev << ")" << std::endl;
+  }
 }
 
 std::string GNB::predict(const std::vector<double> &sample) {
-  /**
-   * Once trained, this method is called and expected to return
-   *   a predicted behavior for the given observation.
-   * @param observation - a 4 tuple with s, d, s_dot, d_dot.
-   *   - Example: [3.5, 0.1, 8.5, -0.2]
-   * @output A label representing the best guess of the classifier. Can
-   *   be one of "left", "keep" or "right".
-   *
-   * TODO: Complete this function to return your classifier's prediction
-   */
+  double max = -1;
+  int index = 0;
+  for(int i=0; i<possible_labels.size(); ++i) {
+    float p = label_prob_[i] *
+      calc_prob(sample[3], d_dot_prob_[i].mean, d_dot_prob_[i].stdev) *
+      calc_prob(sample[1], d_prob_[i].mean, d_prob_[i].stdev) *
+      calc_prob(sample[0], s_prob_[i].mean, s_prob_[i].stdev);
+    if(p > max) {
+      max = p;
+      index = i;
+    }
+  }
+  return possible_labels[index];
+}
 
-  return possible_labels[1];
+gaussian_t GNB::calc_gaussian(std::vector<double>& v) {
+  double sum = std::accumulate(v.begin(), v.end(), 0.0);
+  double mean = sum / v.size();
+
+  std::vector<double> diff(v.size());
+  std::transform(v.begin(), v.end(), diff.begin(), [mean](double x) { return x - mean; });
+  double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  double stdev = std::sqrt(sq_sum / v.size());
+  return gaussian_t{mean,stdev};
+}
+
+double GNB::calc_prob(double x, double ux, double sigmax) {
+  return std::exp(-(x - ux) * (x - ux) / (2 * sigmax * sigmax)) / std::sqrt(2 * M_PI * sigmax * sigmax);
 }
--- 36_prediction/src/classifier.h
+++ 36_prediction/src/classifier.h
@@ -4,7 +4,14 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include "Eigen/Dense"
+#include <iostream>
+#include <numeric>
+#include <iterator>
+
+struct gaussian_t {
+  double mean;
+  double stdev;
+};
 
 // Gaussian Naive Bayes
 class GNB {
@@ -31,6 +38,29 @@ public:
   std::string predict(const std::vector<double> &sample);
 
   std::vector<std::string> possible_labels = {"left","keep","right"};
+
+private:
+  /*
+   * calculates mean and stdev of an array
+   */
+  gaussian_t calc_gaussian(std::vector<double>& v);
+
+  /**
+   * Calculates gaussian probability
+   */
+  double calc_prob(double x, double ux, double sigmax);
+
+  // prior probability p(C_k) for each label
+  std::vector<double> label_prob_;
+
+  // conditional probability for s feature p(s | label)
+  std::vector<gaussian_t> s_prob_;
+
+  // conditional probability for d feature p(d | label)
+  std::vector<gaussian_t> d_prob_;
+
+  // conditional probability for d_dot feature p(d_dot | label)
+  std::vector<gaussian_t> d_dot_prob_;
 };
 
 #endif  // CLASSIFIER_H
